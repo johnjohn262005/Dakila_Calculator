d4ad8479fc84be430f0a24ce3099be77cc3dacc4
STC1 <STC1@DESKTOP-8JRHKC5>
2024-04-16 13:47:13 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        btn1 = New Button()
+        btn2 = New Button()
+        btn3 = New Button()
+        btn4 = New Button()
+        btn5 = New Button()
+        btn6 = New Button()
+        btn7 = New Button()
+        btn8 = New Button()
+        btn9 = New Button()
+        btnplus = New Button()
+        btnminus = New Button()
+        btntimes = New Button()
+        btnequals = New Button()
+        btnclear = New Button()
+        btndivide = New Button()
+        btn0 = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(21, 54)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(264, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' btn1
+        ' 
+        btn1.Location = New Point(21, 104)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(75, 23)
+        btn1.TabIndex = 1
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Location = New Point(118, 104)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(75, 23)
+        btn2.TabIndex = 2
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Location = New Point(210, 104)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(75, 23)
+        btn3.TabIndex = 3
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Location = New Point(21, 154)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(75, 23)
+        btn4.TabIndex = 4
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Location = New Point(118, 154)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(75, 23)
+        btn5.TabIndex = 5
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Location = New Point(210, 154)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(75, 23)
+        btn6.TabIndex = 6
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Location = New Point(21, 207)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(75, 23)
+        btn7.TabIndex = 7
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.Location = New Point(118, 207)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(75, 23)
+        btn8.TabIndex = 8
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Location = New Point(210, 207)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(75, 23)
+        btn9.TabIndex = 9
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btnplus
+        ' 
+        btnplus.Location = New Point(118, 257)
+        btnplus.Name = "btnplus"
+        btnplus.Size = New Size(75, 23)
+        btnplus.TabIndex = 10
+        btnplus.Text = "+"
+        btnplus.UseVisualStyleBackColor = True
+        ' 
+        ' btnminus
+        ' 
+        btnminus.Location = New Point(210, 301)
+        btnminus.Name = "btnminus"
+        btnminus.Size = New Size(75, 23)
+        btnminus.TabIndex = 11
+        btnminus.Text = "-"
+        btnminus.UseVisualStyleBackColor = True
+        ' 
+        ' btntimes
+        ' 
+        btntimes.Location = New Point(210, 257)
+        btntimes.Name = "btntimes"
+        btntimes.Size = New Size(75, 23)
+        btntimes.TabIndex = 12
+        btntimes.Text = "*"
+        btntimes.UseVisualStyleBackColor = True
+        ' 
+        ' btnequals
+        ' 
+        btnequals.Location = New Point(21, 338)
+        btnequals.Name = "btnequals"
+        btnequals.Size = New Size(264, 37)
+        btnequals.TabIndex = 13
+        btnequals.Text = "="
+        btnequals.UseVisualStyleBackColor = True
+        ' 
+        ' btnclear
+        ' 
+        btnclear.Location = New Point(21, 301)
+        btnclear.Name = "btnclear"
+        btnclear.Size = New Size(75, 23)
+        btnclear.TabIndex = 14
+        btnclear.Text = "C"
+        btnclear.UseVisualStyleBackColor = True
+        ' 
+        ' btndivide
+        ' 
+        btndivide.Location = New Point(118, 301)
+        btndivide.Name = "btndivide"
+        btndivide.Size = New Size(75, 23)
+        btndivide.TabIndex = 15
+        btndivide.Text = "/"
+        btndivide.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Location = New Point(21, 257)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(75, 23)
+        btn0.TabIndex = 16
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(232, 26)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 15)
+        Label1.TabIndex = 17
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(305, 57)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(0, 15)
+        Label2.TabIndex = 18
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(301, 407)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(btn0)
+        Controls.Add(btndivide)
+        Controls.Add(btnclear)
+        Controls.Add(btnequals)
+        Controls.Add(btntimes)
+        Controls.Add(btnminus)
+        Controls.Add(btnplus)
+        Controls.Add(btn9)
+        Controls.Add(btn8)
+        Controls.Add(btn7)
+        Controls.Add(btn6)
+        Controls.Add(btn5)
+        Controls.Add(btn4)
+        Controls.Add(btn3)
+        Controls.Add(btn2)
+        Controls.Add(btn1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnplus As Button
+    Friend WithEvents btnminus As Button
+    Friend WithEvents btntimes As Button
+    Friend WithEvents btnequals As Button
+    Friend WithEvents btnclear As Button
+    Friend WithEvents btndivide As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        TextBox1.Text = TextBox1.Text + "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        TextBox1.Text = TextBox1.Text + "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        TextBox1.Text = TextBox1.Text + "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        TextBox1.Text = TextBox1.Text + "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        TextBox1.Text = TextBox1.Text + "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        TextBox1.Text = TextBox1.Text + "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        TextBox1.Text = TextBox1.Text + "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        TextBox1.Text = TextBox1.Text + "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        TextBox1.Text = TextBox1.Text + "9"
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        TextBox1.Text = TextBox1.Text + "0"
+    End Sub
+
+    Private Sub btnplus_Click(sender As Object, e As EventArgs) Handles btnplus.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "+"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub btntimes_Click(sender As Object, e As EventArgs) Handles btntimes.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "*"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub btndivide_Click(sender As Object, e As EventArgs) Handles btndivide.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "/"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub btnminus_Click(sender As Object, e As EventArgs) Handles btnminus.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "-"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub btnclear_Click(sender As Object, e As EventArgs) Handles btnclear.Click
+        TextBox1.Text = ""
+        Label2.Text = ""
+        Label1.Text = ""
+    End Sub
+
+    Private Sub btnequals_Click(sender As Object, e As EventArgs) Handles btnequals.Click
+        If Label2.Text = "+" Then
+            TextBox1.Text = Val(Label1.Text) + Val(TextBox1.Text)
+            Label1.Text = ""
+            Label2.Text = "="
+        End If
+
+        If Label2.Text = "*" Then
+            TextBox1.Text = Val(Label1.Text) * Val(TextBox1.Text)
+            Label1.Text = ""
+            Label2.Text = "="
+        End If
+
+        If Label2.Text = "/" Then
+            TextBox1.Text = Val(Label1.Text) / Val(TextBox1.Text)
+            Label1.Text = ""
+            Label2.Text = "="
+        End If
+
+        If Label2.Text = "-" Then
+            TextBox1.Text = Val(Label1.Text) - Val(TextBox1.Text)
+            Label1.Text = ""
+            Label2.Text = "="
+        End If
+    End Sub
+End Class
